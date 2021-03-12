6a5a13ed9fb0227c5ab899bce179e6d367b13251
williamrm <william.pierre@net-c.com>
2021-03-12 10:41:52 +0100

    correct style

--- src/main/java/com/epsi/mycal/App.java
+++ src/main/java/com/epsi/mycal/App.java
@@ -3,8 +3,7 @@ package com.epsi.mycal;
 public class App {
 
   public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    // TODO Auto-generated method stub
     System.out.println("Hello world");
-	}
-
+  }
 }
