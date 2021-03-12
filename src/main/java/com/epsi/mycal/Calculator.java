c6815bc61fe1d0877c92195a92cb7b1aaa8ca4a4
williamrm <william.pierre@net-c.com>
2021-03-12 10:44:02 +0100

    correct style all corrected

--- src/main/java/com/epsi/mycal/Calculator.java
+++ src/main/java/com/epsi/mycal/Calculator.java
@@ -3,14 +3,14 @@ package com.epsi.mycal;
 public class Calculator {
 
   public int add(int valA, int valB) {
-		return valA+valB;
-	}
+    return valA + valB;
+  }
 
-	public int sub(int valA, int valB) {
-		return valA-valB;
-	}
+  public int sub(int valA, int valB) {
+    return valA - valB;
+  }
 
-	public int mul(int valA, int valB) {
-		return valA*valB;
-	}
+  public int mul(int valA, int valB) {
+    return valA * valB;
+  }
 }
--- src/test/java/com/epsi/mycal/CalculatorTest.java
+++ src/test/java/com/epsi/mycal/CalculatorTest.java
@@ -8,47 +8,46 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+  @Before
+  public void setUp() throws Exception {
+  }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+  @After
+  public void tearDown() throws Exception {
+  }
 
-	@Test
-	public void testAdd() {
-		int a = 2;
-		int b = 3;
-	
+  @Test
+  public void testAdd() {
+    int a = 2;
+    int b = 3;
 
-		Calculator calc = new Calculator();
-		int actual = calc.add(a,b);
+    Calculator calc = new Calculator();
+    int actual = calc.add(a, b);
 
-		assertEquals(5, actual);
-	}
+    assertEquals(5, actual);
+  }
 
-	@Test
-	public void testSub() {
-		int a = 4;
-		int b = 2;
+  @Test
+  public void testSub() {
+    int a = 4;
+    int b = 2;
 
-		Calculator calc = new Calculator();
-		int actual = calc.sub(a,b);
+    Calculator calc = new Calculator();
+    int actual = calc.sub(a, b);
 
-		assertEquals(2, actual);
-	}
-	
-	@Test
-	public void testMul() {
-		int a = 4;
-		int b = 3;
+    assertEquals(2, actual);
+  }
 
-		Calculator calc = new Calculator();
-		int actual = calc.mul(a,b);
+  @Test
+  public void testMul() {
+    int a = 4;
+    int b = 3;
 
-		assertEquals(12, actual);
+    Calculator calc = new Calculator();
+    int actual = calc.mul(a, b);
 
-	}
+    assertEquals(12, actual);
+
+  }
 
 }
